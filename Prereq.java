c5c5cee093daba68cf63675e8c712f107169d8ce
LamDuongT <lduong6@masonlive.gmu.edu>
2018-03-20 17:56:11 -0400

    Minor addition to code

--- Prereq.java
+++ Prereq.java
@@ -1,11 +1,12 @@
 /**
- * 
+ *  Prereq class was made as a container class that contains what prerequisites each course has
  */
 
 /**
  * @author Huan Nguyen
- *
+ * @author Lam Duong
  */
+
 public class Prereq {
 	private int prereqID;
 	private int courseID;
@@ -13,6 +14,7 @@ public class Prereq {
 	
 	
 	public Prereq() {
+		// Constructor method to create empty object
 		setValue(-1, -1, -1);		
 	}
 	
@@ -20,17 +22,16 @@ public class Prereq {
 		setValue(prereqID, courseID, prereqCourseID);		
 	}
 	
-	
 	public void setValue(int prereqID, int courseID, int prereqCourseID) {
 		this.prereqID = prereqID;
 		this.courseID = courseID;
 		this.prereqCourseID = prereqCourseID;
 	}
 	
+	
 	public void setPrereqID (int prereqID) {
 		this.prereqID = prereqID;
 	}
-	
 	public void setCourseID (int courseID) {
 		this.courseID = courseID;
 	}
