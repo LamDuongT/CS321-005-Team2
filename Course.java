85764282e36af5b0a09e8a1ded3cabcdd1863eaa
LamDuongT <lduong6@masonlive.gmu.edu>
2018-03-20 12:10:58 -0400

    Just some template files, to be improved on. First iteration.

---
+++ Course.java
@@ -0,0 +1,15 @@
+import java.sql.*;
+
+public class Course
+{
+	private int courseID;
+	private Prerequisites prereqs;
+	private String courseName;
+	private String description;
+	private int creditHour;
+	private int catalogID;
+	private int MajorID;
+	private int MinorID;
+	
+	public Course(
+	
---
+++ Login.java
@@ -0,0 +1,8 @@
+public class Login
+{
+	private String username;
+	private String password;
+	
+	public int login
+	{
+		
