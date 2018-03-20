4653c9ff8bc0ae377b61f7a91b811ed66cb1316f
hnguye80 <36158584+hnguye80@users.noreply.github.com>
2018-03-20 12:41:24 -0400

    Add files via upload

---
+++ Profile.java
@@ -0,0 +1,95 @@
+/**
+ * 
+ */
+
+/**
+ * @author Huan Nguyen
+ *
+ */
+public class Profile {
+	private int studentID;
+	private String netID;
+	private String studentName;
+	private String studentEmail;
+	private String username;
+	private String password;
+	private String profileName;
+	
+	public Profile() {
+		setValue(-1, "", "", "", "", "", "");
+	}
+	public Profile (int studentID, String netID, String studentName, String studentEmail, String username, String password, String profileName) {
+		setValue(studentID, netID, studentName, studentEmail, username, password, profileName);
+	}
+
+	public void setValue(int studentID, String netID, String studentName, String studentEmail, String username, String password, String profileName) {
+		this.studentID = studentID;
+		this.netID = netID;
+		this.studentName = studentName;
+		this.studentEmail = studentEmail;
+		this.username = username;
+		this.password = password;
+		this.profileName = profileName;
+	}
+	
+	public void setStudentID(int studentID) {
+		this.studentID = studentID;
+	}
+	
+	public void setStudentName (String studentName) {
+		this.studentName = studentName;
+	}
+	
+	public void setStudentEmail(String studentEmail) {
+		this.studentEmail = studentEmail;
+	}
+	
+	public void setUsername (String username) {
+		this.username = username;
+	}
+	
+	public void setPassword (String password) {
+		this.password = password;
+	}
+	
+	public void setProfileName (String profileName) {
+		this.profileName = profileName;
+	}
+	
+	public int getStudentID() {
+		return this.studentID;
+	}
+	
+	public String getStudentName() {
+		return this.studentName;
+	}
+	
+	public String getStudentEmail() {
+		return this.studentEmail;
+	}
+	
+	public String getUsername() {
+		return this.username;
+	}
+	
+	public String getPassword() {
+		return this.password;
+	}
+	
+	public String getProfileName() {
+		return this.profileName;
+	}
+	
+	public String toString() {
+		String returnString = "StudentID = " + this.studentID + "\n";
+		returnString += "netId = " + this.netID + "\n";
+		returnString += "studentName = " + this.studentName + "\n";
+		returnString += "studentEmail = " + this.studentEmail + "\n";
+		returnString += "username = " + this.username + "\n";
+		returnString += "password = " + this.password + "\n";
+		returnString += "profileName = " + this.profileName + "\n";
+		
+		return returnString;
+	}
+	
+}
