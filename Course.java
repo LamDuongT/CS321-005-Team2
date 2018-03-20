e298244933aa65753b03de6cc5e3c0ab8ea591bf
hnguye80 <36158584+hnguye80@users.noreply.github.com>
2018-03-20 14:15:48 -0400

    Add files via upload

--- Course.java
+++ Course.java
@@ -1,3 +1,5 @@
+import java.util.List;
+
 /*
  * To change this license header, choose License Headers in Project Properties.
  * To change this template file, choose Tools | Templates
@@ -11,45 +13,85 @@
  * @author Huan Nguyen
  */
 public class Course {
-    private String classCode;
-    private String className;
-    private String classDescription;
+    private int courseID;
+    private String courseName;
+    private String courseDesc;
     private int creditHours;
-    private Course[] prerequisiteClassID;
+    private int catalogID;
+    private int majorID;
+    private int minorID;
+    private List<Prereqs> prereqList;
     
     public Course() {
-        setClass("", "", "", 0);
+        setValue(-1, "", "", -1, -1, -1, -1);
+        this.prereqList = null;
     }
     
-    public Course (String classCode, String className, String classDescription, int creditHours) {
-        setClass(classCode, className, classDescription, creditHours);
+    public Course (int courseID, String courseName, String courseDesc, int creditHours, int catalogID, int majorID, int minorID) {
+        setValue(courseID, courseName, courseDesc, creditHours, catalogID, majorID, minorID);
+        
+        this.prereqList = new Prereqs(courseID);
     }
     
-    public void setClass(String classCode, String className, String classDescription, int creditHours) {
-        this.classCode = classCode;
-        this.className = className;
-        this.classDescription = classDescription;
-        this.creditHours = creditHours;
-        this.prerequisiteClassID = null;
+    public void setValue(int courseID, String courseName, String courseDesc, int creditHours, int catalogID, int majorID, int minorID) {
+    	this.courseID = courseID;
+    	this.courseName = courseName;
+    	this.courseDesc = courseDesc;
+    	this.creditHours = creditHours;
+    	this.catalogID = catalogID;
+    	this.majorID = majorID;
+    	this.minorID = minorID;
     }
     
-    public String getClassCode() {
-        return this.classCode;
+    public void setCourseID(int courseID) {
+    	this.courseID = courseID;
     }
     
-    public String getClassName() {
-        return this.className;
+    public void setCourseName(String courseName) {
+    	this.courseName = courseName;
     }
     
-    public String getClassDescription() {
-        return this.classDescription;
+    public void setCourseDesc(String courseDesc) {
+    	this.courseDesc = courseDesc;
+    }
+    
+    public void setCatalogID (int catalogID) {
+    	this.catalogID = catalogID;
+    }
+    
+    public void setMajorID (int majorID) {
+    	this.majorID = majorID;    	
+    }
+    
+    public void setMinorID (int minorID) {
+    	this.minorID = minorID;    	
+    }
+    
+    public int getCourseID() {
+        return this.courseID;
+    }
+    
+    public String getCourseName() {
+        return this.courseName;
+    }
+    
+    public String getCourseDesc() {
+        return this.courseDesc;
     }
     
     public int getCreditHours() {
         return this.creditHours;
     }
     
-    public Course[] getpresequisiteClassID() {
-        return this.prerequisiteClassID;
+    public int getCatalogID() {
+    	return this.catalogID;
+    }
+
+    public int getMajorID() {
+    	return this.majorID;
+    }
+    
+    public int getMinorID() {
+    	return this.minorID;
     }
 }
---
+++ Prereq.java
@@ -0,0 +1,53 @@
+/**
+ * 
+ */
+
+/**
+ * @author Huan Nguyen
+ *
+ */
+public class Prereq {
+	private int prereqID;
+	private int courseID;
+	private int prereqCourseID;
+	
+	
+	public Prereq() {
+		setValue(-1, -1, -1);		
+	}
+	
+	public Prereq(int prereqID, int courseID, int prereqCourseID) {
+		setValue(prereqID, courseID, prereqCourseID);		
+	}
+	
+	
+	public void setValue(int prereqID, int courseID, int prereqCourseID) {
+		this.prereqID = prereqID;
+		this.courseID = courseID;
+		this.prereqCourseID = prereqCourseID;
+	}
+	
+	public void setPrereqID (int prereqID) {
+		this.prereqID = prereqID;
+	}
+	
+	public void setCourseID (int courseID) {
+		this.courseID = courseID;
+	}
+	
+	public void setPrereqCourseID (int prereqCourseID) {
+		this.prereqCourseID = prereqCourseID;
+	}
+	
+	public int getPrereqID() {
+		return this.prereqID;
+	}
+	
+	public int getCourseID() {
+		return this.courseID;
+	}
+	
+	public int getPrereqCourseID() {
+		return this.prereqCourseID;
+	}
+}
