7ae80813ed89b43e433799cf0a7a695a2d1ce5b9
hnguye80 <36158584+hnguye80@users.noreply.github.com>
2018-03-22 11:41:16 -0400

    Add Semester classes
    Semester list now can be retrieved from database

---
+++ Semesters.java
@@ -0,0 +1,120 @@
+import java.sql.ResultSet;
+import java.sql.SQLException;
+import java.sql.Statement;
+import java.util.LinkedList;
+import java.util.List;
+
+/**
+ * 
+ */
+
+/**
+ * @author Huan Nguyen
+ *
+ */
+public class Semesters {
+	private List<Semester> semesterList = new LinkedList<Semester>();
+
+	private Semester aSemester;
+	
+	public Semesters() {
+		getSemesterData();
+	}
+	
+	/**
+	 * Get Semester by ID from the list of Semesters
+	 */
+	public Semester getSemesterByID(int semesterID) {
+		aSemester = null;
+		boolean isFound = false;
+
+		for (int index = 0; index < this.semesterList.size(); index++) {
+			aSemester = this.semesterList.get(index);
+			if (aSemester.getSemesterID() == semesterID) {
+				isFound = true;
+				break;
+			}
+		}
+		if (!isFound)
+			aSemester = new Semester();
+
+		return aSemester;
+	}
+
+	/**
+	 * Get Semester by Name from the list of Semesters
+	 */
+	public Semester getCatalogByName(String catalogName) {
+		aSemester = null;
+		boolean isFound = false;
+
+		for (int index = 0; index < this.semesterList.size(); index++) {
+			aSemester = this.semesterList.get(index);
+			if (aSemester.getSemesterName().toLowerCase().indexOf(catalogName) != -1) {
+				isFound = true;
+				break;
+			}
+		}
+		if (!isFound)
+			aSemester = new Semester();
+
+		return aSemester;
+	}
+
+	/**
+	 * fetch all data from the table Semester and add to the list
+	 */
+	private void getSemesterData() {
+		ConnectDB connectdb = new ConnectDB();
+
+		try {
+			int semesterID;
+			String catalogName;
+			String catalogDesc;
+			int creditMin;
+			int creditMax;
+
+			String queryString = "SELECT semesterID, semesterName, semesterDesc, creditMin, creditMax FROM tblsemester ";
+			queryString += "ORDER BY semesterID ASC";
+
+			System.out.println(queryString);
+
+			// Initialize a sql statement
+			Statement statement = connectdb.theConnection.createStatement();
+			// recordSet will hold a data table as sql object
+			// to see how the data table look like, copy the queryString contents and
+			// execute in mysql Workbench
+			ResultSet recordSet = statement.executeQuery(queryString);
+
+			while (recordSet.next()) {
+				semesterID = recordSet.getInt("semesterID");
+				catalogName = recordSet.getString("semesterName");
+				catalogDesc = recordSet.getString("semesterDesc");
+				creditMin = recordSet.getInt("creditMin");
+				creditMax = recordSet.getInt("creditMax");
+
+				aSemester = new Semester(semesterID, catalogName, catalogDesc, creditMin, creditMax);
+				this.semesterList.add(aSemester);
+			}
+			statement.close();
+
+		} catch (SQLException e) {
+			throw new IllegalStateException("[ERROR] there is an error with the sql querry!", e);
+		} finally {
+			connectdb.disconectDB();
+		}
+	}
+
+	/**
+	 * Override toString method for testing purpose
+	 */
+	public String toString() {
+		String returnString = "semesterID | semesterName   | semesterDesc\t\t | credtiMin  | creditMax\n";
+		for (int index = 0; index < this.semesterList.size(); index++) {
+			aSemester = this.semesterList.get(index);
+			returnString += aSemester.toString() + "\n";
+		}
+
+		return returnString;
+	}
+}
