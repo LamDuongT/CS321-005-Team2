ab550b8e2ad554fd6660e6a3573cf9dee0597feb
LamDuongT <lduong6@masonlive.gmu.edu>
2018-03-31 16:24:36 -0400

    Added constructor and getValue

--- Plans.java
+++ Plans.java
@@ -4,9 +4,30 @@ import java.sql.Statement;
 import java.util.LinkedList;
 
 /**
- *@author Lam Duong
+ * @author Lam Duong
  */
 
 public class Plans {
-	private LinkedList<Plan> plansList = new LinkedList<Plan>;
+	private LinkedList<Plan> plansList = new LinkedList<Plan();
+	private Plan aPlan;
+	
+	public Plans() {
+		getPlansData();
+	}
+	
+	/**
+	 * Fetch all of the plans data from the table Plan and add to plansList
+	 */
+	public getPlansData() {
+		ConnectDB connectdb = new ConnectDB();
+		try {
+			int _planID;
+			String _planName;
+			int _major1ID;
+			int _minor1ID;
+			int _major2ID;
+			int _minor2ID;
+			int _catalogID;
+		}
+	}
 }
