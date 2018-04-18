6c9f36376d57951da0a30529e191dd99140ca3d5
hnguye80 <36158584+hnguye80@users.noreply.github.com>
2018-04-17 20:34:46 -0400

    modified the query to get credittakenID

--- Database/getCourseForPlan.sql
+++ Database/getCourseForPlan.sql
@@ -1,8 +1,9 @@
-use collegespdb;
 SELECT plan.planID, plan.catalogID, plan.majorID, plan.minorID, plan.majorID2, plan.minorID2, 
 		profile.studentID, profile.profileName, 
-        course.courseID, course.courseName, credit.semesterID
+        course.courseID, course.courseName, credit.semesterID,
+        credit.creditstakenID
 FROM tblplan plan INNER JOIN tblcreditstaken credit ON plan.profileID = credit.studentID
 	 INNER JOIN tblcourse course on course.courseID = credit.courseID
-     INNER JOIN tblprofile profile on plan.profileID = profile.studentID
-WHERE planID = 2
+     INNer JOIN tblprofile profile on plan.profileID = profile.studentID
+WHERE planID = 2;
+
