269a120836a4322c78a07003166052c5b9f36c1a
LamDuongT <lduong6@masonlive.gmu.edu>
2018-04-18 15:48:27 -0400

    Changed query for the getPlanCreditsTaken() method

--- Plan.java
+++ Plan.java
@@ -119,12 +119,12 @@ public class Plan {
 		CreditsTaken planCreditsTaken = new CreditsTaken();
 		String query = "SELECT plan.planID, plan.catalogID, plan.majorID, plan.minorID, plan.majorID2, plan.minorID2,"
 				+ "profile.studentID, profile.profileName, course.courseID, course.courseName, credit.semesterID,"
-				+ "se.semesterName, se.creditMax, se.creditMin"
+				+ " creditsTakenID "
 				+ "FROM tblplan plan INNER JOIN tblcreditstaken credit ON plan.profileID = credit.studentID"
 				+ "INNER JOIN tblcourse course on course.courseID = credit.courseID"
 				+ "INNer JOIN tblprofile profile on plan.profileID = profile.studentID"
-				+ "INNER JOIN tblsemester se ON se.semesterDesc = credit.semesterID" 
 				+ "WHERE plan.planID = " + this.getPlanID();
+
 		try (Statement statement = connectDB.theConnection.createStatement()) {
 			ResultSet recordSet = statement.executeQuery(query);
 			while(recordSet.next()) {
