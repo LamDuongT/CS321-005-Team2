670d536746caac84e068aed2d6c6d6b03de68c84
LamDuongT <lduong6@masonlive.gmu.edu>
2018-04-25 12:24:28 -0400

    Finished adding in database entry for presentation :) Thanks to everyone involved in this project!

--- Database/Data/collegespdb_tblprereq.sql
+++ Database/Data/collegespdb_tblprereq.sql
@@ -1,6 +1,6 @@
 -- MySQL dump 10.13  Distrib 5.7.17, for Win64 (x86_64)
 --
--- Host: 127.0.0.1    Database: collegespdb
+-- Host: localhost    Database: collegespdb
 -- ------------------------------------------------------
 -- Server version	5.7.21-log
 
@@ -16,12 +16,31 @@
 /*!40111 SET @OLD_SQL_NOTES=@@SQL_NOTES, SQL_NOTES=0 */;
 
 --
+-- Table structure for table `tblprereq`
+--
+
+DROP TABLE IF EXISTS `tblprereq`;
+/*!40101 SET @saved_cs_client     = @@character_set_client */;
+/*!40101 SET character_set_client = utf8 */;
+CREATE TABLE `tblprereq` (
+  `prereqID` int(11) NOT NULL AUTO_INCREMENT,
+  `courseID` int(11) DEFAULT NULL,
+  `prereqCourseID` int(11) DEFAULT NULL,
+  PRIMARY KEY (`prereqID`),
+  KEY `prereq_course_idx` (`courseID`),
+  KEY `prereq_prereq_idx` (`prereqCourseID`),
+  CONSTRAINT `prereq_course` FOREIGN KEY (`courseID`) REFERENCES `tblcourse` (`courseID`) ON DELETE NO ACTION ON UPDATE NO ACTION,
+  CONSTRAINT `prereq_prereq` FOREIGN KEY (`prereqCourseID`) REFERENCES `tblcourse` (`courseID`) ON DELETE NO ACTION ON UPDATE NO ACTION
+) ENGINE=InnoDB AUTO_INCREMENT=30 DEFAULT CHARSET=utf8 COMMENT='Prerequisite Courses		';
+/*!40101 SET character_set_client = @saved_cs_client */;
+
+--
 -- Dumping data for table `tblprereq`
 --
 
 LOCK TABLES `tblprereq` WRITE;
 /*!40000 ALTER TABLE `tblprereq` DISABLE KEYS */;
-INSERT INTO `tblprereq` VALUES (1,3,2),(2,4,2),(3,4,3),(4,5,1),(5,6,3),(6,6,34),(7,7,6),(8,7,81),(9,8,3),(10,8,36),(11,9,4),(12,9,36),(13,10,6),(14,10,9),(15,11,6),(16,11,8),(17,11,36),(18,12,6),(19,12,9),(20,12,39),(21,22,6),(22,22,9),(23,24,6),(24,24,9);
+INSERT INTO `tblprereq` VALUES (1,3,2),(2,4,2),(3,4,3),(4,5,1),(5,6,3),(6,6,34),(7,7,6),(8,7,81),(9,8,3),(10,8,36),(11,9,4),(12,9,36),(13,10,6),(14,10,9),(15,11,6),(16,11,8),(17,11,36),(18,12,6),(19,12,9),(20,12,39),(21,22,6),(22,22,9),(23,24,6),(24,24,9),(25,184,183),(26,186,185),(27,189,35),(28,190,189),(29,81,179);
 /*!40000 ALTER TABLE `tblprereq` ENABLE KEYS */;
 UNLOCK TABLES;
 /*!40103 SET TIME_ZONE=@OLD_TIME_ZONE */;
@@ -34,4 +53,4 @@ UNLOCK TABLES;
 /*!40101 SET COLLATION_CONNECTION=@OLD_COLLATION_CONNECTION */;
 /*!40111 SET SQL_NOTES=@OLD_SQL_NOTES */;
 
--- Dump completed on 2018-04-08 21:40:21
+-- Dump completed on 2018-04-25 12:22:03
